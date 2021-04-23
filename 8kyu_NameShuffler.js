3924a8596fe71ac33dda480f86b01426d98120b4
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-04-23 15:29:02 -0600

    Create 8kyu_NameShuffler.js

---
+++ 8kyu_NameShuffler.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/559ac78160f0be07c200005a
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"
+
+*/
+
+const nameShuffler = str => str.split(' ').reverse().join(' ');
