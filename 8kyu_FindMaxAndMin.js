009abfff02dd283b5bf2f5f3d82cdf2e55446e62
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-04-23 15:21:58 -0600

    Create 8kyu_FindMaxAndMin.js

---
+++ 8kyu_FindMaxAndMin.js
@@ -0,0 +1,48 @@
+/*
+https://www.codewars.com/kata/577a98a6ae28071780000989
+
+Your task is to make two functions, max and min (maximum and minimum in PHP and Python, maxi and mini in Julia) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
+
+#Examples
+
+max([4,6,2,1,9,63,-134,566]) returns 566
+min([-52, 56, 30, 29, -54, 0, -110]) returns -110
+max([5]) returns 5
+min([42, 54, 65, 87, 0]) returns 0
+#Notes
+
+You may consider that there will not be any empty arrays/vectors.
+
+var min = function(list){
+    
+    return list[0];
+}
+
+var max = function(list){
+    
+    return list[0];
+}
+
+*/
+
+function max(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  console.log(max);
+  return max;
+}
+
+function min(arr) {
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  console.log(min);
+  return min;
+}
