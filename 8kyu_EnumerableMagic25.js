ccc0ce5cf6c85c669d25acee566d6fc29103066c
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-04-23 15:32:47 -0600

    Create 8kyu_EnumerableMagic25.js

---
+++ 8kyu_EnumerableMagic25.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/545afd0761aa4c3055001386
+
+Create a method take that accepts a list/array and a number n, and returns a list/array array of the first n elements from the list/array.
+
+*/
+
+
+const take = (arr, n) => arr.slice(0, n);
