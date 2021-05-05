41463483ce83ade9b7badf7cfa1e41448f9429e8
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-05-05 10:17:14 -0600

    Create 8kyu_CalcAve.py

---
+++ 8kyu_CalcAve.py
@@ -0,0 +1,12 @@
+'''
+https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+'''
+
+
+def find_average(numbers):
+    return sum(numbers) / len(numbers)
