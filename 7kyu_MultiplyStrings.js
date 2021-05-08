c620c1554cc6894b9d76f4b16f1bbbc854fe8419
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-05-08 05:13:09 -0600

    Create 7kyu_MultiplyStrings.js

---
+++ 7kyu_MultiplyStrings.js
@@ -0,0 +1,18 @@
+/*
+
+https://www.codewars.com/kata/59b2963132779166d2001018
+
+You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+
+
+*/
+
+const arrMultiply = ([a, b]) => `${a * b}`;
+
+/*
+function arrMultiply(arr){
+   return (Number(arr[0]) * Number(arr[1])).toString();   
+}
+
+*/
