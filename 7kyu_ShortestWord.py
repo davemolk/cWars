d10b9e719630c8b96ff9fb3861080d09aec84f3b
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-05-16 06:05:56 -0600

    Create 7kyu_ShortestWord.py

---
+++ 7kyu_ShortestWord.py
@@ -0,0 +1,16 @@
+'''
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+'''
+
+
+def find_short(s):
+    return min(len(x) for x in s.split(' '))
+  
+'''
+def find_short(s):
+    arr = s.split(' ')
+    word = min(arr, key=len)
+    return len(word)
+'''    
