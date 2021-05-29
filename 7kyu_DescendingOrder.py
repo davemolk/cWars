fe16ed7f4d1eb7ff8a4fa29c81338f2514c37a98
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-05-29 09:43:45 -0600

    Create 7kyu_DescendingOrder.py

---
+++ 7kyu_DescendingOrder.py
@@ -0,0 +1,9 @@
+'''
+https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
+Essentially, rearrange the digits to create the highest possible number.
+
+'''
+def descending_order(num):
+    return int(''.join(sorted(list(str(num)), reverse=True)))
