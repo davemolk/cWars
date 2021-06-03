f21a9de5bef8ae34df5601520dee136b4f22a5d0
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-06-03 08:53:21 -0600

    Create 7kyu_AllStar1.py

---
+++ 7kyu_AllStar1.py
@@ -0,0 +1,8 @@
+'''
+https://www.codewars.com/kata/5863f97fb3a675d9a700003f/python
+
+Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG
+
+'''
+def sum_ppg(playerOne, playerTwo):
+    return playerOne["ppg"] + playerTwo["ppg"]
